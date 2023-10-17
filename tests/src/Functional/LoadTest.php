3379539534cdcba4ab9aae2816ae9e21a58a7a5e
parkhatou <parkh@ohio.edu>
2023-10-17 13:01:18 -0400

    Update LoadTest.php

--- tests/src/Functional/LoadTest.php
+++ tests/src/Functional/LoadTest.php
@@ -20,6 +20,11 @@ class LoadTest extends BrowserTestBase {
   public static $modules = ['views_cm_current_state'];
 
   /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+  
+  /**
    * A user with permission to administer site configuration.
    *
    * @var \Drupal\user\UserInterface
